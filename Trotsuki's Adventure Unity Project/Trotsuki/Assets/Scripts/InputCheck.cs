d4b4ce019701e597f526ace58b4100952e9caa77
green2onion <green2onion@users.noreply.github.com>
2018-09-27 11:59:16 -0700

    InputCheck prototype

--- Trotsuki's Adventure Unity Project/Trotsuki/Assets/Scripts/InputCheck.cs
+++ Trotsuki's Adventure Unity Project/Trotsuki/Assets/Scripts/InputCheck.cs
@@ -8,19 +8,46 @@ public class InputCheck : MonoBehaviour
 	private GameObject[] choiceObjects;
 	private string[] choiceObjectsStrings;
 	private int charPos;
-	private void checkInput()
+	private string currentString;
+	private void CheckInput()
 	{
 		foreach (char inputChar in Input.inputString) // get input char
 		{
+			char inputCharLower = char.ToLower(inputChar);
 			for (int i = 0; i < choiceObjectsStrings.Length; i++) // try every choice
 			{
-				if (inputChar == choiceObjectsStrings[i][charPos]) // check if the input char == the char at charPos
+				if (inputCharLower == choiceObjectsStrings[i].ToLower()[charPos]) // check if the input char == the char at charPos
 				{
-					charPos++;
+					print(choiceObjectsStrings[i][charPos]);
+					currentString = currentString + choiceObjectsStrings[i][charPos];
+					if (currentString == choiceObjectsStrings[i])
+					{
+						InputSuccess(i);
+					}
+					else
+					{
+						charPos++;
+					}
+				}
+				else
+				{
+					if (inputCharLower!='\0')
+					{
+						WrongInput(inputCharLower);
+					}
+					
 				}
 			}
-
 		}
+	}
+	private void InputSuccess(int choice)
+	{
+		currentString = "";
+		print("zuccess!");
+	}
+	private void WrongInput(char input)
+	{
+		print("wrong input!" + input);
 	}
 
 	// Use this for initialization
@@ -32,12 +59,18 @@ public class InputCheck : MonoBehaviour
 		{
 			choiceObjects[i] = gameObject.transform.GetChild(i).gameObject;
 		}
+		choiceObjectsStrings = new string[4];
+		for (int i = 0; i < 4; i++)
+		{
+			choiceObjectsStrings[i] = choiceObjects[i].GetComponent<TextMesh>().text;
+		}
 
 	}
 
 	// Update is called once per frame
 	private void Update()
 	{
-		checkInput();
+		CheckInput();
+		print(Input.inputString);
 	}
 }
