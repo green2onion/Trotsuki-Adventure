a42370515fbff40b36410941a5575dcc99e20dc4
green2onion <green2onion@users.noreply.github.com>
2018-09-27 11:13:07 -0700

    reorganized folders

---
+++ Trotsuki's Adventure Unity Project/Trotsuki/Assets/Scripts/InputCheck.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputCheck : MonoBehaviour
+{
+	public List<string> choiceStrings;
+	private GameObject[] choiceObjects;
+	private string[] choiceObjectsStrings;
+	private int charPos;
+	private void checkInput()
+	{
+		foreach (char inputChar in Input.inputString) // get input char
+		{
+			for (int i = 0; i < choiceObjectsStrings.Length; i++) // try every choice
+			{
+				if (inputChar == choiceObjectsStrings[i][charPos]) // check if the input char == the char at charPos
+				{
+					charPos++;
+				}
+			}
+
+		}
+	}
+
+	// Use this for initialization
+	private void Start()
+	{
+		choiceObjects = new GameObject[4];
+		charPos = 0;
+		for (int i = 0; i < 4; i++)
+		{
+			choiceObjects[i] = gameObject.transform.GetChild(i).gameObject;
+		}
+
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+		checkInput();
+	}
+}
