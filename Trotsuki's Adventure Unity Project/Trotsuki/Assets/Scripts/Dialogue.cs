1191cca3cdba20a87669add145b5aa0de4ade514
green2onion <green2onion@users.noreply.github.com>
2018-10-25 12:55:43 -0700

    read csv now

---
+++ Trotsuki's Adventure Unity Project/Trotsuki/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue 
+{
+	public int id;
+	public string dialogueText;
+	public string[] choices;
+
+	public void SetDialogueText(string dialogueText, int index) // normalize the dialogue, 0 = opponent, 1234 = choices
+	{
+		this.dialogueText = dialogueText.Replace("COMMA", ",");
+	}
+
+}
---
+++ Trotsuki's Adventure Unity Project/Trotsuki/Assets/Scripts/LoadDialogue.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadDialogue : MonoBehaviour
+{
+	private TextAsset dialogueData;
+	private string[] dialogues;
+	public List<Dialogue> dialogueList;
+	// Use this for initialization
+	private void Start()
+	{
+		dialogueList = new List<Dialogue>();
+		dialogueData = Resources.Load<TextAsset>("story"); // load the csv
+		dialogues = dialogueData.text.Split(new char[] { '\n' }); // split by row
+
+
+		for (int i = 1; i < dialogues.Length - 1; i++) // for each row
+		{
+			string[] row = dialogues[i].Split(new char[] { ',' }); // split by column
+			Dialogue dialogue = new Dialogue(); // Dialogue is a class that contains all the dialogue info
+			int.TryParse(row[0], out dialogue.id); // copy the id from the column into the dialogue instance
+			//print(row[1]);
+			dialogue.SetDialogueText(row[1]); // copy the dialogueText from the column into the dialogue instance
+			dialogueList.Add(dialogue); // add the dialogue to our list
+		}
+
+		foreach(Dialogue dialogue in dialogueList)
+		{
+			print(dialogue.id + dialogue.dialogueText);
+		}
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+
+	}
+}
